39b57fdabbc0f4417ccf755a4e57f4b973d08295
Franco <francolobos3@gmail.com>
2023-12-28 02:06:41 +0100

    remixRunBoilerPlate

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,27 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "NinmbusHub" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className=" bg-white py-6 sm:py-8 lg:py-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div className="mb-10 md:mb-16">
+          <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Welcome to NimbusHub</h2>
+          <p className="mb-4 text-center">
+            This is a simple app to demonstrate the power of{" "}
+            <a href="https://remix.run">Remix</a>.
+          </p>
+        </div>
+
+        <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:">
+
+        </div>
+      </div>
     </div>
   );
 }
